2dcfb56b4d9a3ece4afcbb5ff97656d6626f4532
daranda97 <48872258+daranda97@users.noreply.github.com>
2020-02-03 17:09:53 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,14 +8,17 @@ import java.net.URL;
 public class Main {
 	
 	public static void main(String[] args) throws Exception  {
+		
 		URL path = Main.class.getResource("File.txt");
 		File file = new File(path.getFile());
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String line; 
+		
 		while ((line = br.readLine()) != null) {
 			System.out.println(line); 
 		 } 
 		
+		br.close();
 	}
 
 }
