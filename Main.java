932dd7218b2012ec40c3fa80c1ed13cd0a441c5c
daranda97 <48872258+daranda97@users.noreply.github.com>
2020-02-03 17:08:29 -0700

    Main class reads txt file

--- Main.java
+++ Main.java
@@ -1,4 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
 
 public class Main {
+	
+	public static void main(String[] args) throws Exception  {
+		URL path = Main.class.getResource("File.txt");
+		File file = new File(path.getFile());
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line; 
+		while ((line = br.readLine()) != null) {
+			System.out.println(line); 
+		 } 
+		
+	}
 
 }
