3bcfd24a26bb2d212158fd29feedbd63af50ed79
Nick <nick41991.s@gmail.com>
2020-02-14 09:33:44 -0700

    SyntaxToken class for storing data

---
+++ SyntaxToken.java
@@ -0,0 +1,15 @@
+public class SyntaxToken {
+	String type;
+	String image;
+
+	public SyntaxToken(){
+		type = null;
+		image = null;
+	}
+
+	public SyntaxToken (String t, String i){
+		type = t;
+		image = i;
+	}
+
+}
