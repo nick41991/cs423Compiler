3f2de109d147785ea674bb57f830cc914c7ea3ef
Nick <nick41991.s@gmail.com>
2020-04-05 13:17:26 -0600

    Update to flatten multiplication and divison
    THis second stage of flattening converts placeholders with more than one operation into placeholders with one operation.

--- Intermediate.java
+++ Intermediate.java
@@ -27,7 +27,7 @@ public class Intermediate
 
 	private void dfsprint(Node node, int height){
 		int i;
-		for(i = 0; i < height; i++) {
+		for(i = 0; i < height - 1; i++) {
 			System.out.print("\t");
 		}
 		if(height != 0){
@@ -36,7 +36,7 @@ public class Intermediate
 			System.out.println(node.getPayload());
 		}
 		for(Node n : node.children) {
-			for(i = 0; i < height + 1; i++) {
+			for(i = 0; i < height; i++) {
 				System.out.print("\t");
 			}
 			System.out.println("|");
@@ -68,38 +68,83 @@ public class Intermediate
 
 	/*Expression handling function*/
 	private void flatten(Node root, int height){
-		if((root.getPayload().equals("Expression"))){
-			//If a node is an expression, we will create a placeholder for it
 
-			//Make expression string
-			String exp = "";
-			for(Node c : root.children){
-				if(c.getPayload().equals("Expression")){
-					flatten(c, height + 1);
-				}
-				exp = exp.concat(c.getPayload());
+		//If a node is an expression, we will create a placeholder for it
+
+		//Make expression string
+		String exp = "";
+		for(Node c : root.children){
+			if(c.getPayload().equals("Expression")){
+				flatten(c, height + 1);
 			}
-
-			Placeholder expression = new Placeholder("L" + ph_num, exp, height);
-			placeholders.add(expression);
-			root.setPayload(expression.name);
-			root.children.clear();
-			ph_num++;
-			//System.out.println(root.getPayload() + ", " + height);
-		} else {
-			//System.out.print(root.getPayload());
+			exp = exp.concat(c.getPayload());
 		}
-		// for(Node c : root.children){
-		// 	flatten(c, height + 1);
-		// }
+
+		Placeholder expression = new Placeholder("L" + ph_num, exp, height);
+		placeholders.add(expression);
+		root.setPayload(expression.name);
+		root.children.clear();
+		ph_num++;
+		//System.out.println(root.getPayload() + ", " + height);
+
 	}
 
 	/*Placeholders with mutilplication and division can hold more than one
 	operation due to the tree structure.
 	Here, they will be ironed into 3 addr holders.*/
 	private void ph_flatten(){
+		boolean modified = false;
+		int op_count;
+		char cur;
+		ArrayList<Placeholder> newPlaceholders = new ArrayList<Placeholder>();
 		for(Placeholder p : placeholders){
-
+			op_count = 0;
+			/*A count of operators tells us how many operations occur within a ph*/
+			for(int i = 0; i < p.expression.length(); i++){
+				cur = p.expression.charAt(i);
+				switch(cur){
+					case '+':
+					case '-':
+					case '*':
+					case '/':
+						op_count++;
+					default:
+						break;
+				}
+			}
+			
+			if(op_count > 1){
+				modified = true;
+				String exp;
+				int ops = 0;
+				/*Determine index for substring*/
+				int j;
+				for(j = 0; j < p.expression.length() && ops != 2; j++){
+					cur = p.expression.charAt(j);
+					switch(cur){
+						case '+':
+						case '-':
+						case '*':
+						case '/':
+							ops++;
+						default:
+							break;
+					}
+				}
+				/*Create new placeholder*/
+				exp = p.expression.substring(0, j - 1);
+				Placeholder sub_expression = new Placeholder("L" + ph_num, exp, p.height + 1);
+				p.expression = sub_expression.name.concat(p.expression.substring(j - 1));
+				newPlaceholders.add(sub_expression);
+				ph_num++;
+			}
 		}
+		if(modified){
+			for(Placeholder p : newPlaceholders){
+				placeholders.add(p);
+			}
+			ph_flatten();
+		}
+
 	}
 }
